537d3d86b9d88b871fcfae4449c931e9accdf6f0
Mattson Compton <mcompton@Mattsons-MacBook-Pro.local>
2019-01-22 16:16:39 -0700

    finished assigment

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import { timingSafeEqual } from 'crypto';
+import {Link} from 'react-router-dom'
+
+
 
 export default class Student extends Component {
   constructor() {
     super()
 
+    this.state = {
+      studentInfo: {}
+    }
+    this.goBack = this.goBack.bind(this)
   }
-
+  componentDidMount(){
+    axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+    .then(response => {this.setState({studentInfo: response.data})})
+  }
+  goBack(){
+    this.props.history.goBack();
+  }
   render() {
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
+        <button onClick = {() => this.goBack()}>Back</button>
+    
       </div>
     )
   }
